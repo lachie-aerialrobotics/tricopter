7bca42b0e179c6b36baed758fa788daa08d7ce48
lachie-aerialrobotics <lachlan.orr15@imperial.ac.uk>
2022-10-18 00:39:20 +0100

    state machine buggy but working with new state machine package

--- src/print_manager.py
+++ src/print_manager.py
@@ -13,100 +13,99 @@ from mavros_msgs.srv import CommandTOL
 from trajectory_msgs.msg import MultiDOFJointTrajectory
 from tf.transformations import euler_from_quaternion
 from tricopter.srv import *
-
-import statemachine as st
 from transitions import Machine
-
 from trajectory_handler import *
 
-class printStateMachine(st.StateMachine):
-    rate = 30
+class printStateMachine(object):
+    states = ['Idle', 'Takeoff', 'Landing', 'Hover', 'MoveToPrint', 'MoveToPad', 'Print', 'Manual']
 
-    #define all drone states
-    Idle = st.State("Idle", initial=True)
-    Takeoff = st.State("Takeoff")
-    Landing = st.State("Landing")
-    Hover = st.State("Hover")
-    Move = st.State("Move")
-    Print = st.State("Print")
+    transitions = [
+        {'trigger': 'startTakeoff',     'source': 'Idle',       'dest': 'Takeoff', 'before': 'on_Takeoff'},
+        {'trigger': 'finishTakeoff',    'source': 'Takeoff',    'dest': 'Hover', 'after': 'on_Hover'},
+        {'trigger': 'goToPrint',        'source': 'Hover',      'dest': 'MoveToPrint', 'before': 'on_goToPrint'},
+        {'trigger': 'arriveAtPrint',    'source': 'MoveToPrint','dest': 'Print'},
+        {'trigger': 'returnToPad',      'source': 'Print',      'dest': 'MoveToPad', 'before': 'on_return'},
+        {'trigger': 'arriveAtPad',      'source': 'MoveToPad','dest': 'Hover'},
+        {'trigger': 'startLanding',     'source': 'Hover',      'dest': 'Landing', 'before': 'on_Landing'},
+        {'trigger': 'finishLanding',    'source': 'Landing',    'dest': 'Idle'}
+    ]
+    def __init__(self):
+        self.tol_speed = 0.5
+        self.takeoff_hgt = 0.5
+        self.target = FlatTarget()
+        self.yaw = 0.0
+        self.layer = 0
 
-    #define state transitions
-    startTakeoff = Idle.to(Takeoff)
-    finishTakeoff = Takeoff.to(Hover)
-    goToPrint = Hover.to(Move)
-    arriveAtPrint = Move.to(Print)
-    returnToPad = Print.to(Move)
-    arriveAtPad = Move.to(Hover)
-    startLanding = Hover.to(Landing)
-    finishLanding = Landing.to(Idle)
+        self.machine = Machine(model=self, states=self.states, transitions=self.transitions, initial = 'Idle')
 
-    layer = 0
-
-    def __init__(self, model=None, state_field='state', start_value=None):
-        super().__init__(model=None, state_field='state', start_value=None)
-        # timer callback to send setpoints at a reasonable rate    
-        sp_timer = rospy.Timer(rospy.Duration(1.0/self.rate), self._timer_cb, reset=True)
-
-    #callbacks on entering states
-    def on_enter_Takeoff(self):
+    #callbacks on state transitions
+    def on_Takeoff(self):
         rospy.loginfo("Takeoff initiated")
         self.pad_pose = PoseStamped()
-        self.pad_pose = pM.local_pose
-        self.pad_pose.pose.position.z = pM.takeoff_hgt
+        self.pad_pose = self.local_pose
+        self.pad_pose.pose.position.z = self.takeoff_hgt
 
-    def on_enter_Hover(self):              
+    def on_Hover(self):              
         rospy.loginfo("generating trajectory for next layer")
-        pM.dronepath, pM.toolpath = tH.generate_print_layer(self.layer)    
-        self.goToPrint()
+        self.dronepath, self.toolpath = tH.generate_print_layer(self.layer)    
+        pM.printState.goToPrint()
     
-    def on_enter_Landing(self):
+    def on_Landing(self):
         rospy.loginfo("Landing initiated")
           
-    # callbacks on state transitions
-    def on_returnToPad(self):
+    def on_return(self):
         rospy.loginfo("generating trajectory to home")
-        pM.transitionpath = tH.generate_transition(pM.local_pose, self.pad_pose)  
+        self.transitionpath = tH.generate_transition(self.local_pose, self.pad_pose)  
 
     def on_goToPrint(self): 
         rospy.loginfo("generating trajectory to beginning of print")    
-        pM.transitionpath = tH.generate_transition(pM.local_pose, tH.get_print_start_pose(pM.dronepath))
+        self.transitionpath = tH.generate_transition(self.local_pose, tH.get_print_start_pose(self.dronepath))
 
     # callbacks to occur on timer event
+    def during_Hover(self):
+        pass
+
     def during_Takeoff(self):
         #increase target z to deined loiter height
-        if pM.target.position.z < pM.takeoff_hgt:
-            pM.target.position.z += pM.tol_speed / pM.rate
+        if self.target.position.z < self.takeoff_hgt:
+            self.target.position.z += self.tol_speed / self.rate
         else: #when target has reached loiter height and drone knows its flying, move to next state 
-            pM.target.position.z = pM.takeoff_hgt
-            if pM.mavros_ext_state.landed_state == 2:
-                self.finishTakeoff()
+            self.target.position.z = self.takeoff_hgt
+            if self.mavros_ext_state.landed_state == 2:
+                pM.printState.finishTakeoff()
 
     def during_Idle(self):
         #set target position to current landed position to prevent big jumps
         header = Header(stamp=rospy.Time.now(), frame_id="map")
-        pM.target, pM.yaw = flat_target_msg_conversion(pM.local_pose, TwistStamped(header=header), TwistStamped(header=header), 4)
+        self.target, self.yaw = flat_target_msg_conversion(self.local_pose, TwistStamped(header=header), TwistStamped(header=header), 4)
         #wait for arming and offboard then takeoff
-        if (pM.mavros_state.mode == "OFFBOARD") and pM.mavros_state.armed:
-            self.startTakeoff()
+        if (self.mavros_state.mode == "OFFBOARD") and self.mavros_state.armed:
+            pM.printState.startTakeoff()
 
-    def during_Move(self):
-        pose, velocity, acceleration, complete = tH.follow(pM.transitionpath)
-        pM.target, pM.yaw = flat_target_msg_conversion(pose, velocity, acceleration, 2)
+    def during_MoveToPrint(self):
+        pose, velocity, acceleration, complete = tH.follow(self.transitionpath)
+        self.target, self.yaw = flat_target_msg_conversion(pose, velocity, acceleration, 2)
         if complete:
-            self.arriveAtPrint()
+            pM.printState.arriveAtPrint()
+
+    def during_MoveToPad(self):
+        pose, velocity, acceleration, complete = tH.follow(self.transitionpath)
+        self.target, self.yaw = flat_target_msg_conversion(pose, velocity, acceleration, 2)
+        if complete:
+            pM.printState.arriveAtPad()
 
     def during_Print(self):
-        pose, velocity, acceleration, complete_drone = tH.follow(pM.dronepath)
-        pM.target, pM.yaw = flat_target_msg_conversion(pose, velocity, acceleration, 2)
-        tooltip_pose, tooltip_twist, tooltip_accel, complete_tooltip = tH_tooltip.follow(pM.toolpath)
+        pose, velocity, acceleration, complete_drone = tH.follow(self.dronepath)
+        self.target, self.yaw = flat_target_msg_conversion(pose, velocity, acceleration, 2)
+        tooltip_pose, tooltip_twist, tooltip_accel, complete_tooltip = tH_tooltip.follow(self.toolpath)
         pM.pub_tooltip_pose.publish(tooltip_pose)
         pM.pub_tooltip_twist.publish(tooltip_twist)
         if complete_drone and complete_tooltip:
-            self.returnToPad()
+            pM.printState.returnToPad()
 
     def during_Landing(self):
-        if pM.target.position.z > 0 and not pM.mavros_ext_state.landed_state == 1:
-            pM.target.position.z += -pM.tol_speed / self.rate
+        if self.target.position.z > 0 and not self.mavros_ext_state.landed_state == 1:
+            self.target.position.z += -self.tol_speed / self.rate
         else:
             rospy.wait_for_service('/mavros/cmd/land')
             try:
@@ -114,26 +113,18 @@ class printStateMachine(st.StateMachine):
                 l(altitude = 0)
             except rospy.ServiceException:
                 rospy.loginfo("Landing service call failed") 
-            self.finishLanding()
-
-    def _timer_cb(self, event):
-        exec("self.during_" + str(self.current_state.identifier) + "()")
-
+            pM.printState.finishLanding()
 
 class printManager:
     def __init__(self):
         self.rate = 30
-        self.tol_speed = 0.5
-        self.takeoff_hgt = 0.5
-        self.target = FlatTarget()
-        self.yaw = 0.0
+        
+
+        self.printState = printStateMachine()
 
         self.dronepath = MultiDOFJointTrajectory()
         self.toolpath = MultiDOFJointTrajectory()
         self.transitionpath = MultiDOFJointTrajectory()
-
-        # create state machine instance
-        self.droneState = printStateMachine(start_value='Idle')
 
         # publishers to geometric controller
         self.geo_pose_pub = rospy.Publisher(
@@ -151,44 +142,48 @@ class printManager:
 
         # drone state subscriber
         state_sub = rospy.Subscriber(
-            '/mavros/state', State, self.state_cb, queue_size=1, tcp_nodelay=True)
+            '/mavros/state', State, self._state_cb, queue_size=1, tcp_nodelay=True)
         ext_state_sub = rospy.Subscriber(
-            '/mavros/extended_state', ExtendedState, self.ext_state_cb, queue_size=1, tcp_nodelay=True)
+            '/mavros/extended_state', ExtendedState, self._ext_state_cb, queue_size=1, tcp_nodelay=True)
         local_position_sub = rospy.Subscriber(
-            '/mavros/local_position/pose', PoseStamped, self.local_pos_cb, queue_size=1, tcp_nodelay=True)
+            '/mavros/local_position/pose', PoseStamped, self._local_pos_cb, queue_size=1, tcp_nodelay=True)
 
         # wait for drone to come online
         rospy.wait_for_message('/mavros/state', State)
         rospy.wait_for_message('/mavros/extended_state', ExtendedState)
         rospy.wait_for_message('/mavros/local_position/pose', PoseStamped)
 
-        sp_timer = rospy.Timer(rospy.Duration(1.0/self.rate), self.sp_timer_cb, reset=True)
+        # timer callback to send setpoints at a reasonable rate    
+        sp_timer = rospy.Timer(rospy.Duration(1.0/self.rate), self._timer_cb, reset=True)
 
-        def sp_timer_cb(self, event):
-            self.geo_pose_pub.publish(self.target)
-            self.geo_yaw_pub.publish(Float32(self.yaw))
 
-            if self.droneState.current_state == self.droneState.Idle:
-                tooltip_state = "RETRACTED"
-            elif self.droneState.current_state == self.droneState.Hover:
-                tooltip_state = "STAB_3DOF"
-            elif self.droneState.current_state == self.droneState.Move:
-                tooltip_state = "STAB_3DOF"
-            elif self.droneState.current_state == self.droneState.Print:
-                tooltip_state = "STAB_6DOF"
-            else:
-                tooltip_state = "HOME"
-            
-            self.pub_tooltip_state.publish(String(tooltip_state))
+    def _timer_cb(self, event):
+        exec("self.printState.during_" + str(self.printState.state) + "()")
 
-    def state_cb(self, state_msg):
-        self.mavros_state = state_msg
+        self.geo_pose_pub.publish(self.printState.target)
+        self.geo_yaw_pub.publish(Float32(self.printState.yaw))
 
-    def ext_state_cb(self, ext_state_msg):
-        self.mavros_ext_state = ext_state_msg
+        # if self.droneState.current_state == self.droneState.Idle:
+        #     tooltip_state = "RETRACTED"
+        # elif self.droneState.current_state == self.droneState.Hover:
+        #     tooltip_state = "STAB_3DOF"
+        # elif self.droneState.current_state == self.droneState.Move:
+        #     tooltip_state = "STAB_3DOF"
+        # elif self.droneState.current_state == self.droneState.Print:
+        #     tooltip_state = "STAB_6DOF"
+        # else:
+        #     tooltip_state = "HOME"
+        
+        # self.pub_tooltip_state.publish(String(tooltip_state))
 
-    def local_pos_cb(self, local_pose_msg):
-        self.local_pose = local_pose_msg
+    def _state_cb(self, state_msg):
+        self.printState.mavros_state = state_msg
+
+    def _ext_state_cb(self, ext_state_msg):
+        self.printState.mavros_ext_state = ext_state_msg
+
+    def _local_pos_cb(self, local_pose_msg):
+        self.printState.local_pose = local_pose_msg
 
 
 def flat_target_msg_conversion(pose, velocity, acceleration, mask):
