9e59a4708992c340d1e4625786107dc46f5bd924
lachie-aerialrobotics <lachlan.orr15@imperial.ac.uk>
2023-03-10 17:25:28 +0000

    tf header changes

--- scripts/vicon_align.py
+++ scripts/vicon_align.py
@@ -12,7 +12,8 @@ class ViconServer:
     def __init__(self):
         rospy.init_node('vicon_service')
         # services for trajectory generation
-        
+        self.tfBuffer = tf2_ros.Buffer(2.0)
+        listener = tf2_ros.TransformListener(self.tfBuffer)
         vicon_service = rospy.Service(
             'align_vicon', alignVicon, self.align_vicon)
         rospy.spin()
@@ -20,39 +21,48 @@ class ViconServer:
     def align_vicon(self, req):
         rospy.loginfo("Vicon alignment requested")
 
-        mavros_pose_topic = "/mavros/local_position/pose"
-        vicon_pose_topic = str("/vicon/"+req.object_name+"/pose")
-        print("Topic is "+vicon_pose_topic)
-        self.vicon_sub = rospy.Subscriber(vicon_pose_topic, PoseStamped, self.vicon_cb)
-        self.mavros_sub = rospy.Subscriber(mavros_pose_topic, PoseStamped, self.mavros_cb)
+        mavros_pose_frame = "base_link"
+        vicon_pose_frame = str(req.object_name)
+
+        # print("Topic is "+vicon_pose_topic)
+        # self.vicon_sub = rospy.Subscriber(vicon_pose_topic, PoseStamped, self.vicon_cb)
+        # self.mavros_sub = rospy.Subscriber(mavros_pose_topic, PoseStamped, self.mavros_cb)
         
-        rospy.loginfo("Waiting for pose data...")
-        rospy.wait_for_message(mavros_pose_topic, PoseStamped)
-        rospy.wait_for_message(vicon_pose_topic, PoseStamped)
-        rospy.wait_for_message('/Odometry', Odometry)
-        rospy.loginfo("Got pose data!")
-        rospy.loginfo("Waiting 5s for odom data to settle...")
-        rospy.sleep(5)
+        # rospy.loginfo("Waiting for pose data...")
+        # rospy.wait_for_message(mavros_pose_topic, PoseStamped)
+        # rospy.wait_for_message(vicon_pose_topic, PoseStamped)
+        # rospy.wait_for_message('/Odometry', Odometry)
+        # rospy.loginfo("Got pose data!")
+        # rospy.loginfo("Waiting 5s for odom data to settle...")
+        # rospy.sleep(5)
 
-        mavros_vec = np.asarray([self.mavros_pose_msg.pose.position.x, self.mavros_pose_msg.pose.position.y, self.mavros_pose_msg.pose.position.z])
-        mavros_quat = [self.mavros_pose_msg.pose.orientation.x, self.mavros_pose_msg.pose.orientation.y, self.mavros_pose_msg.pose.orientation.z, self.mavros_pose_msg.pose.orientation.w]
-        mavros_rot = R.from_quat(mavros_quat)
+        while not got_tf:
+            try:
+                tf_mav2vicon = self.tfBuffer.lookup_transform(mavros_pose_frame, vicon_pose_frame, rospy.time.now(), timeout=rospy.Duration(1))
+                got_tf = True
+            except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
+                rospy.loginfo("tf dropped - retrying..")
 
-        vicon_vec = np.asarray([self.vicon_pose_msg.pose.position.x, self.vicon_pose_msg.pose.position.y, self.vicon_pose_msg.pose.position.z])
-        vicon_quat = [self.vicon_pose_msg.pose.orientation.x, self.vicon_pose_msg.pose.orientation.y, self.vicon_pose_msg.pose.orientation.z, self.vicon_pose_msg.pose.orientation.w]
-        vicon_rot = R.from_quat(vicon_quat)
+        # mavros_vec = np.asarray([self.mavros_pose_msg.pose.position.x, self.mavros_pose_msg.pose.position.y, self.mavros_pose_msg.pose.position.z])
+        # mavros_quat = [self.mavros_pose_msg.pose.orientation.x, self.mavros_pose_msg.pose.orientation.y, self.mavros_pose_msg.pose.orientation.z, self.mavros_pose_msg.pose.orientation.w]
+        # mavros_rot = R.from_quat(mavros_quat)
 
-        mav2vic_vec = -vicon_vec + mavros_vec
-        mav2vic_rot = mavros_rot.inv() * vicon_rot
-        mav2vic_quat = mav2vic_rot.as_quat()
+        # vicon_vec = np.asarray([self.vicon_pose_msg.pose.position.x, self.vicon_pose_msg.pose.position.y, self.vicon_pose_msg.pose.position.z])
+        # vicon_quat = [self.vicon_pose_msg.pose.orientation.x, self.vicon_pose_msg.pose.orientation.y, self.vicon_pose_msg.pose.orientation.z, self.vicon_pose_msg.pose.orientation.w]
+        # vicon_rot = R.from_quat(vicon_quat)
+
+        # mav2vic_vec = -vicon_vec + mavros_vec
+        # mav2vic_rot = mavros_rot.inv() * vicon_rot
+        # mav2vic_quat = mav2vic_rot.as_quat()
 
         br_static = tf2_ros.StaticTransformBroadcaster()
         tf = TransformStamped()
         tf.header.frame_id = "map"
         tf.header.stamp = rospy.Time.now()
         tf.child_frame_id = "mocap"
-        tf.transform.translation = Vector3(mav2vic_vec[0], mav2vic_vec[1], mav2vic_vec[2])
-        tf.transform.rotation = Quaternion(mav2vic_quat[0], mav2vic_quat[1], mav2vic_quat[2], mav2vic_quat[3])
+        tf.transform = tf_mav2vicon.transform
+        # tf.transform.translation = Vector3(mav2vic_vec[0], mav2vic_vec[1], mav2vic_vec[2])
+        # tf.transform.rotation = Quaternion(mav2vic_quat[0], mav2vic_quat[1], mav2vic_quat[2], mav2vic_quat[3])
         br_static.sendTransform(tf)
 
         rospy.loginfo("Alignment complete - static tf has been published")
@@ -62,13 +72,13 @@ class ViconServer:
          
         return resp
 
-    def mavros_cb(self, msg):
-        rospy.loginfo("Retreived pose in odom frame")
-        self.mavros_pose_msg = msg
+    # def mavros_cb(self, msg):
+    #     rospy.loginfo("Retreived pose in odom frame")
+    #     self.mavros_pose_msg = msg
 
-    def vicon_cb(self, msg):
-        rospy.loginfo("Retreived pose in mocap frame")
-        self.vicon_pose_msg = msg
+    # def vicon_cb(self, msg):
+    #     rospy.loginfo("Retreived pose in mocap frame")
+    #     self.vicon_pose_msg = msg
         
     
 if __name__ == "__main__":
